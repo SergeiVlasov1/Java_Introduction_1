daffaa13484f8d36c96f78d2f754dd178000922b
SergeiVlasov1 <vlasovsergei1988@gmail.com>
2022-02-09 19:46:30 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int bill = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = bill - transfer;
+        System.out.println(total);
+    }
+}
